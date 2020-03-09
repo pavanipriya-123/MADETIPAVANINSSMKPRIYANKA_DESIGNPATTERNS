677aeb35d2baa630ec1f4bc6974465fc94276cf4
pavanipriya-123 <17pa1a0484@vishnu.edu.in>
2020-03-10 00:20:16 +0530

    first commit

---
+++ adapter/BirdAdapter.java
@@ -0,0 +1,15 @@
+package com.adapter.adapter;
+class BirdAdapter implements ToyDuck 
+{ 
+    Bird bird; 
+
+    public void squeak() 
+    { 
+        bird.makeSound(); 
+    } 
+    public BirdAdapter(Bird bird) 
+    { 
+        this.bird = bird; 
+    } 
+  
+} 
---
+++ adapter/ToyDuck.java
@@ -0,0 +1,6 @@
+package com.adapter.adapter;
+
+public interface Bird {
+	public void fly(); 
+    public void makeSound(); 
+}
